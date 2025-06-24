8d22baeaa2dc8f73bfbaab7f14e51a09d62221f6
Estefania0711 <laeslobu@gmail.com>
2025-06-23 23:54:09 -0500

    11:54pm

--- Agrosellnova/src/main/java/com/agrosellnova/Agrosellnova/controladores/PaginaController.java
+++ Agrosellnova/src/main/java/com/agrosellnova/Agrosellnova/controladores/PaginaController.java
@@ -1,6 +1,8 @@
 package com.agrosellnova.Agrosellnova.controladores;
 
 import jakarta.servlet.http.HttpSession;
+import org.apache.catalina.filters.ExpiresFilter;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -15,11 +17,34 @@ public class PaginaController {
         model.addAttribute("usuario", usuario);
 
         // Puedes restringir algunas páginas si es necesario
-        if (pagina.equals("cerrar-sesion") || pagina.equals("login") || pagina.equals("api")) {
+        if (pagina.equals("cerrar_sesion") || pagina.equals("index") || pagina.equals("api")) {
             return "redirect:/error";
         }
 
         // Retorna la vista desde templates/public/pagina.html
         return "public/" + pagina;
+    }
+
+    @GetMapping("/{pagina}")
+    public String mostrarPaginaForms(@PathVariable("pagina") String pagina, HttpSession session, Model model) {
+        String usuario = (String) session.getAttribute("usuario");
+        model.addAttribute("usuario", usuario);
+
+        if (pagina.equals("cerrar_sesion") || pagina.equals("index")) {
+            return "redirect:/error";
+        }
+
+        return "form/" + pagina;
+    }
+
+    @GetMapping("/{pagina}")
+    public String mostrarPaginaPrivate(@PathVariable("pagina") String pagina, HttpSession session, Model model) {
+        String usuario = (String) session.getAttribute("usuario");
+        model.addAttribute("usuario", usuario);
+
+        if (pagina.equals("cerrar_sesion") || pagina.equals("index")) {
+            return "redirect:/error";
+        }
+        return "form/" + pagina;
     }
 }
