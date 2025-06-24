a1c871ce9cf66ddaa74114ba82faf7b138fe8c88
ismel <ssismel28@gmail.com>
2025-06-23 20:49:41 -0500

    first commit

---
+++ Agrosellnova/src/main/java/com/agrosellnova/Agrosellnova/AgrosellnovaApplication.java
@@ -0,0 +1,13 @@
+package com.agrosellnova.Agrosellnova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgrosellnovaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgrosellnovaApplication.class, args);
+	}
+
+}
