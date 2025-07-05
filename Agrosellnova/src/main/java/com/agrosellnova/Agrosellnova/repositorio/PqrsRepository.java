bf43b29badfa1a0f86dd3cca0c0f823770803c25
KarolPuello <karolpuellog0819@gmail.com>
2025-07-04 23:03:00 -0500

    filtros admin - karol

--- Agrosellnova/src/main/java/com/agrosellnova/Agrosellnova/repositorio/PqrsRepository.java
+++ Agrosellnova/src/main/java/com/agrosellnova/Agrosellnova/repositorio/PqrsRepository.java
@@ -2,14 +2,10 @@ package com.agrosellnova.Agrosellnova.repositorio;
 
 import com.agrosellnova.Agrosellnova.modelo.Pqrs;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
-@Repository
 public interface PqrsRepository extends JpaRepository<Pqrs, Long> {
     List<Pqrs> findByNombreContainingIgnoreCase(String nombre);
     List<Pqrs> findByCorreoContainingIgnoreCase(String correo);
     List<Pqrs> findByTelefonoContainingIgnoreCase(String telefono);
-
 }
