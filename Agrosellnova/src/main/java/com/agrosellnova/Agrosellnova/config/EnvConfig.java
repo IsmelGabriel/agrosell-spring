7c12a56726507d03fb9cf1a20b8e4deb4198f3af
Ismel Salazar <ssismel28@gmail.com>
2025-11-21 12:19:07 -0500

    configura envconfig para deploy en render

--- Agrosellnova/src/main/java/com/agrosellnova/Agrosellnova/config/EnvConfig.java
+++ Agrosellnova/src/main/java/com/agrosellnova/Agrosellnova/config/EnvConfig.java
@@ -2,6 +2,7 @@ package com.agrosellnova.Agrosellnova.config;
 
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import javax.annotation.PostConstruct;
 
@@ -10,9 +11,13 @@ public class EnvConfig {
 
     @PostConstruct
     public void loadEnv() {
-        Dotenv dotenv = Dotenv.load();
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing() // importante por si acaso
+                .load();
+
         dotenv.entries().forEach(e ->
                 System.setProperty(e.getKey(), e.getValue())
         );
     }
 }
+
